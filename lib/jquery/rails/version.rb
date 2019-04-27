7dd9165858cdf8e739885d3f513b67af3164cf8c
shamangeorge <shamangeorge@fruitopology.net>
2019-04-27 12:12:36 +0000

    add jquery migrate for possible migrations.

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -5,5 +5,7 @@ module Jquery
     JQUERY_2_VERSION = "2.2.4"
     JQUERY_3_VERSION = "3.4.0"
     JQUERY_UJS_VERSION = "1.2.2"
+    JQUERY_MIGRATE_1x_VERSION = "1.4.1"
+    JQUERY_MIGRATE_3x_VERSION = "3.0.1"
   end
 end
